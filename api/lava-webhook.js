440fffcf1cb92c86fe88539308ee1b9d0c7e5db9
KudusSMP <vanuckinaleksej@gmail.com>
2026-05-06 20:53:44 +0300

    Update lava-webhook.js

--- api/lava-webhook.js
+++ api/lava-webhook.js
@@ -1,7 +1,7 @@
 import { Rcon } from 'rcon-client';
 
 const RCON_HOST = process.env.RCON_HOST;
-const RCON_PORT = Number(process.env.RCON_PORT);
+const RCON_PORT = Number(process.env.RCON_PORT || 25575);
 const RCON_PASSWORD = process.env.RCON_PASSWORD;
 
 const commands = {
@@ -13,40 +13,58 @@ const commands = {
 
 export default async function handler(req, res) {
   try {
-    if (req.method !== 'POST') {
-      return res.status(405).send('Method not allowed');
-    }
+if (req.method === 'GET') {
 
+  const product = 'NOOB';
+  const nick = 'testerhuy';
+
+  const commandTemplate = commands[product];
+  const command = commandTemplate.replace('{nick}', nick);
+
+  const rcon = await Rcon.connect({
+    host: RCON_HOST,
+    port: RCON_PORT,
+    password: RCON_PASSWORD
+  });
+
+  await rcon.send(command);
+  await rcon.end();
+
+  return res.status(200).send('Тестовая выдача выполнена');
+}
     const body = req.body;
 
     console.log('Lava webhook:', body);
 
-    const status = String(body.status || body.invoiceStatus || body.paymentStatus || '').toLowerCase();
+    const status = String(body.status || '').toLowerCase();
 
-    if (
-      status &&
-      !['success', 'paid', 'completed', 'succeeded'].includes(status)
-    ) {
-      return res.status(200).send('payment not completed');
+    if (status !== 'success') {
+      return res.status(200).send('payment not success');
     }
 
-    let customFields = body.customFields;
+    const orderId = body.order_id || body.orderId;
 
-    if (typeof customFields === 'string') {
-      customFields = JSON.parse(customFields);
+    if (!orderId) {
+      return res.status(400).send('Нет order_id');
     }
 
-    const product = customFields?.product;
-    const nick = customFields?.nick;
+    const parts = orderId.split('-');
+
+    const product = parts[0];
+    const nick = parts[1];
 
     if (!product || !nick) {
-      return res.status(400).send('Нет product или nick');
+      return res.status(400).send('Ошибка чтения order_id');
     }
 
     const commandTemplate = commands[product];
 
     if (!commandTemplate) {
       return res.status(400).send('Неизвестный товар');
+    }
+
+    if (!RCON_HOST || !RCON_PORT || !RCON_PASSWORD) {
+      return res.status(500).send('Не заданы RCON переменные в Vercel');
     }
 
     const command = commandTemplate.replace('{nick}', nick);
@@ -61,10 +79,12 @@ export default async function handler(req, res) {
     await rcon.end();
 
     console.log(`Выдан донат: ${product} игроку ${nick}`);
+    console.log(`Команда: ${command}`);
 
     return res.status(200).send('ok');
   } catch (error) {
-    console.error(error);
+    console.error('Webhook error:', error);
+
     return res.status(500).send(error.message);
   }
 }
